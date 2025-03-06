46c470c3d568588d221b93a9a723114c409802d2
Neybi <neybi028@gmail.com>
2025-03-05 21:13:20 -0400

    Modificaciones y correciones

--- application/controllers/admin/Clients.php
+++ application/controllers/admin/Clients.php
@@ -20,7 +20,57 @@ class Clients extends CI_Controller {
     }
 
     // this function allows to edit a client
-   
+    public function edit($id = NULL) {
+        if ($id) {
+            $data ['client'] = $this->clients_m->get($id);
+            $data['municipio'] = $this->clients_m->get_editMunicipio($data['client']->provincia_id);
+            $data['sector'] = $this->clients_m->get_editSector($data['client']->municipio_id);
+            
+            $data ['client'] || show_404();
+        } else {
+            $data ['client'] = $this->clients_m->get_new();
+        }
+        
+        $data['provincia'] = $this->clients_m->get_provincia();
+
+        $rules = $this ->clients_m->clients_rules;
+        $this->form_validation->set_rules($rules);
+
+        if ($this->form_validation->run() == TRUE) {
+            $cst_data = $this->clients_m->array_from_post(['dni','first_name', 'last_name', 'gender', 'provincia_id', 'municipio_id', 'sector_id', 'mobile', 'address', 'phone', 'business_name', 'ruc', 'company']);
+            
+            // Check if DNI already exists
+            if ($this->clients_m->dni_exists($cst_data['dni'], $id)) {
+                
+                $this->session->set_flashdata('error', 'La cédula ingresada ya existe. Por favor, use uno diferente.');
+            } else {
+                $this->clients_m->save($cst_data, $id);
+      
+                if($id){
+                    $this->session->set_flashdata('msg', 'El cliente ha sido actualizado');
+                }else{
+                    $this->session->set_flashdata('msg', 'El cliente ha sido creado');
+                }
+            
+                redirect('admin/clients');
+            }
+            
+            
+        }
+            
+        $data['subview'] = 'admin/clients/edit';
+        $this->load->view('admin/_main_layout', $data);
+    }
+
+    public function ajax_getMunicipios($prov_id) {
+        echo $this ->clients_m->get_municipio($prov_id);
+    }
+
+    public function ajax_getSectores($mp_id) {
+        echo $this ->clients_m->get_sector($mp_id);
+    }
+    
+    
     
 }
 
