0a7de9230c382bce7338e7be065377ba31b38906
Neybi <neybi028@gmail.com>
2025-03-07 03:33:10 -0400

    modificaciones y correciones

--- application/models/Loans_m.php
+++ application/models/Loans_m.php
@@ -30,7 +30,49 @@ class Loans_m extends MY_Model
     }
 
     // this function is used to get all coins from the database
-   
+    public function get_coins(){
+        return $this->db->get('coins')->result();
+    }
+
+    // this function is used to get a client by its dni
+    public function get_searchCst($dni){
+        $this->db->where('dni', $dni);
+        return $this->db->get('clients')->row();
+    }
+
+    // this function is used to add a new loan to the database
+    public function add_loan($data, $items){
+        // insert loan data to loans table
+        if($this->db->insert('loans', $data)){
+            $loan_id = $this->db->insert_id();
+            
+            $this->db->where('id', $data['client_id']);
+            $this->db->update('clients', ['loan_status'=> 1]);
+            foreach ($items as $item) {
+                $item['loan_id'] = $loan_id;
+                $this->db->insert('loan_items', $item);
+            }
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    //get loan by id
+    public function get_loan($loan_id){
+        $this->db->select("l.*, CONCAT(c.first_name, ' ', c.last_name) AS client_name, co.short_name");
+        $this->db->from('loans l');
+        $this->db->join('clients c', 'c.id = l.client_id', 'left');
+        $this->db->join('coins co', 'co.id = l.coin_id', 'left');
+        $this->db->where('l.id', $loan_id);
+
+        return $this->db->get()->row();
+     }
+
+     public function get_loanItems($loan_id){
+        $this->db->where('loan_id', $loan_id);
+
+        return $this->db->get('loan_items')->result(); 
+    }
     // this function is used to get all payments from the database
     // public function get_payments($loan_id){
     //     $this->db->where('loan_id', $loan_id);
