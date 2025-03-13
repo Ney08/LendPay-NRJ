19438e32bc371da92390db46ed40e497103c4efe
Neybi <neybi028@gmail.com>
2025-03-13 02:45:40 -0400

    get

--- application/models/Coins_m.php
+++ application/models/Coins_m.php
@@ -23,7 +23,15 @@ class Coins_m extends MY_Model {
         ),
     );
 
-    
+    public function get_new() {
+        $coin = new stdClass();
+        $coin->name = '';
+        $coin->short_name = '';
+        $coin->symbol = '';
+        $coin->description = '';
+
+        return $coin;
+    }
 
     
 }
